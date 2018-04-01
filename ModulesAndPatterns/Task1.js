82b261042fefd5256eb44c9eceea359872ae94fd
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-04-01 22:15:05 +0300

    Update Task1.js

--- ModulesAndPatterns/Task1.js
+++ ModulesAndPatterns/Task1.js
@@ -1,9 +1,14 @@
+/*jslint devel: true */
+/*jslint es6 */
+'use strict';
+
 function solve() {
     let courseName = '',
         presentationsNames = [],
         students = [],
         homeworks = [],
-        results = [];
+        examResults = [],
+        studentID = 0;
     const Course = {
         init: function (title, presentations) {
             if (title.length < 1 || title[0] === ' ' || title[title.length - 1] === ' ' || title.search(/\s{2,}/g) !== -1) {
@@ -27,14 +32,39 @@ function solve() {
             if (fullname[0].search(/[A-Z]/) !== 0 || fullname[1].search(/[A-Z]/) !== 0) {
                 throw name + ' : Invalid student name';
             }
-
-            return id;
+            studentID++;
+            let student = {
+                firstname: fullname[0],
+                lastname: fullname[1],
+                id: studentID
+            };
+            students.push(student);
+            return studentID;
         },
         getAllStudents: function () {
-
+            return students;
         },
         submitHomework: function (studentID, homeworkID) {
-
+            let isPassStudentID = false,
+                isPassHomeworkID = false;
+            students.forEach(element => {
+                if (element.id === studentID) {
+                    isPassStudentID = true;
+                }
+            });
+            presentationsNames.forEach((el, index) => {
+                if (index + 1 === homeworkID) {
+                    isPassHomeworkID = true;
+                }
+            });
+            if (!isPassStudentID || !isPassHomeworkID) {
+                throw 'Invalid student or homework ID';
+            }
+            let homework = {
+                studentID: studentID,
+                homeworkID: homeworkID
+            }
+            homeworks.push(homework);
         },
         pushExamResults: function (results) {
 
@@ -45,7 +75,10 @@ function solve() {
     };
     return Course;
 }
-let course1 = solve();
-let presents = ['Lection 1', 'Lection 2', 'Lection 3'];
+let course1 = solve(),
+    presents = ['Lection 1', 'Lection 2', 'Lection 3'];
 course1.init('Javascript Fundamentals', presents);
 course1.addStudent('Lili Ivanova');
+course1.addStudent('Gosho Patkanov');
+console.log(course1.getAllStudents());
+course1.submitHomework(1, 1);
