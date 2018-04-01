6a6580c26fb952e8230c3320f46aef0366aa57eb
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-04-01 13:23:49 +0300

    Update Task1.js

--- ModulesAndPatterns/Task1.js
+++ ModulesAndPatterns/Task1.js
@@ -1,24 +1,19 @@
-/*jslint devel: true */
-/*jslint es6 */
-'use strict';
-
-const course = (function () {
-    let courseName,
+function solve() {
+    let courseName = '',
         presentationsNames = [],
         students = [],
         homeworks = [],
         results = [];
-
-    return {
+    const Course = {
         init: function (title, presentations) {
             if (title.length < 1 || title[0] === ' ' || title[title.length - 1] === ' ' || title.search(/\s{2,}/g) !== -1) {
-                throw title + ': Invalid course title';
+                throw title + ' : Invalid course title';
             } else if (presentations.length === 0) {
-                throw presentations + ': Missing presentation/s';
+                throw presentations + ' : Missing presentation/s';
             }
             presentations.forEach(element => {
                 if (element.length < 1 || element[0] === ' ' || element[element.length - 1] === ' ' || element.search(/\s{2,}/g) !== -1) {
-                    throw element + ': Invalid presentation title';
+                    throw element + ' : Invalid presentation title';
                 }
             });
             courseName = title;
@@ -30,9 +25,9 @@ const course = (function () {
             let fullname = name.match(/\w+/g),
                 id;
             if (fullname[0].search(/[A-Z]/) !== 0 || fullname[1].search(/[A-Z]/) !== 0) {
-                throw name + ': Invalid student name';
+                throw name + ' : Invalid student name';
             }
-            
+
             return id;
         },
         getAllStudents: function () {
@@ -48,7 +43,9 @@ const course = (function () {
 
         }
     };
-}());
+    return Course;
+}
+let course1 = solve();
 let presents = ['Lection 1', 'Lection 2', 'Lection 3'];
-course.init('Javascript Fundamentals', presents);
-course.addStudent('Lili Ivanova');
+course1.init('Javascript Fundamentals', presents);
+course1.addStudent('Lili Ivanova');
