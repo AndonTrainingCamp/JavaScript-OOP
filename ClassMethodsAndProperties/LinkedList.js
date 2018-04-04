0ebe1e72d8573bcee8c4324d6bf05a0bb42420d5
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-04-04 18:06:40 +0300

    Add files via upload

---
+++ ClassMethodsAndProperties/LinkedList.js
@@ -0,0 +1,25 @@
+/*jslint devel: true */
+/*jslint es6 */
+'use strict';
+
+class listNode {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+    get first() {
+
+    }
+    get last() {
+
+    }
+}
+class LinkedList {
+    constructor() {
+        this._length = 0;
+        this._head = null;
+    }
+    get length() {
+
+    }
+}
