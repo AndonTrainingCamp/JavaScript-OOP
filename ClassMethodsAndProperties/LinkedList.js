f41da9bc5e5982a2007bafc8442e27640576684b
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-04-05 18:34:52 +0300

    Update LinkedList.js

--- ClassMethodsAndProperties/LinkedList.js
+++ ClassMethodsAndProperties/LinkedList.js
@@ -20,6 +20,35 @@ class LinkedList {
         this._head = null;
     }
     get length() {
+        return this._length;
+    }
+    append(...args) {
+        let current;
+        args.forEach((value, index, arr) => {
+            let node = {
+                data: value,
+                next: null
+            };
+            if (this._head === null) {
+                this._head = node;
+                this._length++;
+            } else {
+                current = this._head;
+                while (current.next) {
+                    current = current.next;
+                }
+                current.next = node;
+                this._length++;
+            }
+        });
+    }
+    prepend(els) {
+
+    }
+    insert(index, els) {
 
     }
 }
+let myList = new LinkedList();
+myList.append({ a: 1, b: 2 }, [3, 4], 5.99932, 'Hello 6');
+console.log(myList);
